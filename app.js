f2e2312e687f9026f51f2ca8518f57191eb60cf0
andrew.atwong@accenture.com <andrew.atwong@accenture.com>
2019-01-31 15:26:41 -0800

    uncommented endpoints

--- app.js
+++ app.js
@@ -8,28 +8,27 @@ app.listen(port_number, () => {  // to run server, "node app.js"
     console.log('Server started!');
 });
 
+app.get('/', function(req, res){
+    res.send('<h1>Server is up!</h1>');
+});  
 
-// app.get('/', function(req, res){
-//     res.send('<h1>Server is up!</h1>');
-// });  
 
+app.get('/info', function(req, res){
+    response = {
+        info: 'grapes'
+    }
+    console.log('grapes gotten');
+    res.send(response);
+});  
 
-// app.get('/info', function(req, res){
-//     response = {
-//         info: 'grapes'
-//     }
-//     console.log('grapes gotten');
-//     res.send(response);
-// });  
-
-// app.post('/feedbackMessage', function(req, res) {
-//     console.log('feedback received!');
-//     console.log(req);
-//     console.log(req.body);
-//     const responseBody = {
-//         res: 'got the message',
-//         payload: req.body
-//     }
-//     res.send(responseBody);
-//     res.end();
-// });
+app.post('/feedbackMessage', function(req, res) {
+    console.log('feedback received!');
+    console.log(req);
+    console.log(req.body);
+    const responseBody = {
+        res: 'got the message',
+        payload: req.body
+    }
+    res.send(responseBody);
+    res.end();
+});
